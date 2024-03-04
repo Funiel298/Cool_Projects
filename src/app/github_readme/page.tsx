fd6ad5d4f12dbd5aa70c3233cf0958ef03a2830a
Daniel <91716291+Funiel298@users.noreply.github.com>
2024-03-04 23:26:42 +0500

    please, I really need to sleep

--- src/app/github_readme/page.tsx
+++ src/app/github_readme/page.tsx
@@ -21,17 +21,18 @@ export default function Github(){
 
     return(
         <div>
-            <h1>
+            <h1 className="text-2xl font-bold ml-auto mr-auto text-center mt-5">
                 Github ReadMe
             </h1>
+            <p className="text-sm ml-auto mr-auto text-center">That is app where you will create your own something</p>
             <div className="flex flex-row  w-screen">
                 <textarea
-                    className="w-1/2  min-h-[70vh] max-h-[90vh] m-5 p-10 rounded-2xl border-2 border-black border-solid  "
+                    className="w-1/2  min-h-[70vh] max-h-[80vh] m-5 p-10 rounded-2xl border-2 border-black border-solid  "
                     value={input1}
                     onChange={handleInputChange1}
                     />
                 <textarea
-                    className="w-1/2  min-h-[70vh] max-h-[90vh] m-5 p-10 rounded-2xl border-2 border-black border-solid "
+                    className="w-1/2  min-h-[70vh] max-h-[80vh] m-5 p-10 rounded-2xl border-2 border-black border-solid "
                     value={input2}
                     readOnly
                     />
